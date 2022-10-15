074a89ff523264af7a98ead01108b806ecf578de
Dimboss <mahmud6415@gmail.com>
2022-10-15 19:56:41 +0400

    refreshed

--- src/main/java/az/spring/rest/demo/springrestdemo/controller/RegistrationController.java
+++ src/main/java/az/spring/rest/demo/springrestdemo/controller/RegistrationController.java
@@ -7,7 +7,6 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
